c4ff3a3e65e72b675188e0fdcc8e8f7ab0959ff4
Anthony Niculescu-Cornejo <117235442+Anicor147@users.noreply.github.com>
2023-10-10 15:57:02 -0400

    Created a Dictionnary for WeaponBorder
    IT WORKSSSS

--- Midterm/Assets/Scripts/UI/UIScript.cs
+++ Midterm/Assets/Scripts/UI/UIScript.cs
@@ -12,11 +12,32 @@ public class UIScript : MonoBehaviour
     [SerializeField] private Slider manaSlider;
     [SerializeField] private PlayerController _playerController;
     [SerializeField] private TMP_Text moneyCounter;
+    [SerializeField] private GameObject fireWeapon;
+    [SerializeField] private GameObject iceWeapon;
+    [SerializeField] private GameObject lightningWeapon; 
+    [SerializeField] private GameObject lightningBorder; 
+    [SerializeField] private GameObject iceBorder; 
+    [SerializeField] private GameObject fireBorder;
+    private Dictionary<GameObject, GameObject> weaponBorders = new Dictionary<GameObject, GameObject>();
 
     public void Update()
     {
         PlayerBars();
         PlayerCurrency();
+        CheckWeaponActive();
+    }
+
+    public void Start()
+    {
+        AddToWeaponBorders();
+    }
+
+    public void AddToWeaponBorders()
+    {
+        weaponBorders = new Dictionary<GameObject, GameObject>();
+        weaponBorders.Add(fireWeapon , fireBorder);
+        weaponBorders.Add(iceWeapon , iceBorder);
+        weaponBorders.Add(lightningWeapon , lightningBorder);
     }
 
     public void PlayerBars()
@@ -30,6 +51,13 @@ public class UIScript : MonoBehaviour
         moneyCounter.text = _playerController.Currency.ToString();
     }
 
+    public void CheckWeaponActive()
+    {
+        foreach (var border in weaponBorders)
+        {
+          border.Value.SetActive(border.Key.activeSelf);
+        }
+    }
 
 }
 
