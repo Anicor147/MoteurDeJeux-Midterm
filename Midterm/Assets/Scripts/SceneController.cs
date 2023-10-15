8ec10a066a536d1ca275d36c244f132691ec4a86
Anthony Niculescu-Cornejo <117235442+Anicor147@users.noreply.github.com>
2023-10-14 21:54:09 -0400

    set a spawn point on the Shop Scene

--- Midterm/Assets/Scripts/SceneController.cs
+++ Midterm/Assets/Scripts/SceneController.cs
@@ -9,15 +9,19 @@ public class SceneController : MonoBehaviour
 {
     private GameObject player;
     private PlayerController _playerController;
+    private GameObject _spawnPoint;
     
     private void Start()
     {
+        _spawnPoint = GameObject.FindGameObjectWithTag("Spawn"); 
         player = GameObject.FindGameObjectWithTag("Player");
         _playerController = player.GetComponent<PlayerController>();
         
         _playerController.CurrentHealth = _playerController.MaxHealth;
         _playerController.CurrentMana = _playerController.MaxMana;
         _playerController.Revive();
+
+        _playerController.transform.position = _spawnPoint.transform.position;
 
     }
 
